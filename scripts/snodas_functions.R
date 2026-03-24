93b80eb7c67553786ed954720b0b483caf9e3d1a
jeffmarti <jeffmarti@users.noreply.github.com>
2026-03-24 08:31:19 -0700

    Update snodas_functions.R

--- scripts/snodas_functions.R
+++ scripts/snodas_functions.R
@@ -262,9 +262,10 @@ compute_huc8_swe <- function(swe_raster, huc8, swe_date) {
       HUC8     = huc8,
       Name     = name,
       States   = states,
-      AreaSqKm = areasqkm,
-      AreaAcres = areaacres
+      AreaSqKm = area_km2_clipped
     ) %>%
+    mutate(AreaAcres = AreaSqKm * 247.105) %>%
+    select(HUC8, Name, States, AreaSqKm, AreaAcres) %>%
     select(HUC8, Name, States, AreaSqKm, AreaAcres) %>%
     cbind(swe_mean_mm = round(swe_mean, 2)) %>%
     cbind(swe_min_mm  = round(swe_min,  2)) %>%
